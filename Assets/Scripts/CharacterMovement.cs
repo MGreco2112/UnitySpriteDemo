9e633096fbfe47e1ad7f8a249e2304d2d406b253
Matthew Greco <mgreco2112@tutanota.com>
2023-05-19 22:58:06 -0400

    created script that moves triangle sprite to edge of inner square background along the x axis

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
-    
+    public float speed = 6.0f;
     
     void Start() {
         
@@ -11,6 +11,10 @@ public class CharacterMovement : MonoBehaviour {
 
     
     void Update() {
-        
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaTimeX = deltaX * Time.deltaTime;
+        if (transform.position.x + deltaTimeX < 4.5 && transform.position.x + deltaTimeX > -4.5) {
+            transform.Translate(deltaTimeX, 0, 0);
+        }
     }
 }
